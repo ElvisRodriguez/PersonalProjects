86ea01269b07b9f226b1badd9b0a3cc1637ab4af
Elvis Rodriguez <elvisrodriguez1992@gmail.com>
2018-10-11 01:51:14 +0000

    work in progress

--- BeautSoup/smogon_data.py
+++ BeautSoup/smogon_data.py
@@ -16,10 +16,11 @@ def find_strategies(name):
     link = format_url(name)
     result = requests.get(link)
     if result.status_code == 200:
+        print(name)
         src = result.content
         soup = BeautifulSoup(src, features='html.parser')
         span_tag = soup.find('span', {'data-reactid':'.0.1.1.3.6.0.0'})
-        if span_tag is None:
+        if span_tag is not None:
             div_tag = soup.find('div', {'data-reactid':'.0.1.1.3.6.0.2'})
             p_tags = div_tag.find_all('p')
             with open('gsc_strats.txt', 'a') as file:
@@ -29,7 +30,7 @@ def find_strategies(name):
                 file.close()
         else:
             with open('gsc_strats.txt', 'a') as file:
-                file.write(name + ' has no strategies\n')
+                file.write(name + ':\nNo strategies\n\n')
                 file.close()
 
 def find_all_strategies(names):
@@ -38,5 +39,4 @@ def find_all_strategies(names):
 
 if __name__ == '__main__':
     data = import_data()
-    # find_strategies('abra')
-    # find_all_strategies(data)
+    find_all_strategies(data)
