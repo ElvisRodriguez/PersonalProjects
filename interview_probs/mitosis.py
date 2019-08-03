56feeab0c05ded476a9c3bb56050f2fe33f2a1ab
Elvis Rodriguez <elvisrodriguez1992@gmail.com>
2019-08-03 19:22:31 -0400

    Gave projects proper names, removed unecessary files/directories

--- interview_probs/mitosis.py
+++
@@ -1,7 +0,0 @@
-import sys
-import random
-
-with open(sys.argv[0], "a") as file:
-    file.write("\nwith open(sys.argv[0], 'a') as file:")
-    file.write("\n\tfile.write('\\nprint(random.randint(1,1000))')")
-    file.close()
